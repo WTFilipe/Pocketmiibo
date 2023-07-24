cf5b76c654b3d650e5d1b4929d28754fdc143e3a
Filipe Oliveira <filipe.oliveira@hurb.com>
2023-07-23 23:49:14 -0300

    Starting UI

--- app/src/main/java/com/filipeoliveira/pocketmiibo/MainActivity.kt
+++ app/src/main/java/com/filipeoliveira/pocketmiibo/MainActivity.kt
@@ -3,13 +3,21 @@ package com.filipeoliveira.pocketmiibo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.filipeoliveira.pocketmiibo.ui.AmiiboItem
+import com.filipeoliveira.pocketmiibo.ui.models.AmiiboUI
 import com.filipeoliveira.pocketmiibo.ui.theme.PocketmiiboTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,7 +29,7 @@ class MainActivity : ComponentActivity() {
             PocketmiiboTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Home("Android")
+                    Home()
                 }
             }
         }
@@ -29,17 +37,35 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Home(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Home(modifier: Modifier = Modifier) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        modifier = modifier.padding(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(5) {
+            val amiibo = AmiiboUI(
+                type = "",
+                tail = "",
+                name = "Filipe",
+                image = "https://cdn.awsli.com.br/2500x2500/138/138431/produto/11179849/11ffd2a092.jpg",
+                head = "",
+                amiiboSeries = "",
+                character = "",
+                gameSeries = ""
+            )
+            AmiiboItem(item = amiibo) {
+
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     PocketmiiboTheme {
-        Home("Android")
+        Home()
     }
 }
--- app/src/main/java/com/filipeoliveira/pocketmiibo/ui/AmiiboItem.kt
+++ app/src/main/java/com/filipeoliveira/pocketmiibo/ui/AmiiboItem.kt
@@ -1,38 +1,67 @@
 package com.filipeoliveira.pocketmiibo.ui
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import com.filipeoliveira.pocketmiibo.ui.models.AmiiboUI
 import com.filipeoliveira.pocketmiibo.ui.theme.PocketmiiboTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AmiiboItem(modifier: Modifier = Modifier, name: String, imageURL: String) {
+fun AmiiboItem(modifier: Modifier = Modifier, item: AmiiboUI, onItemClick: () -> Unit) {
 
-    Card(onClick = { /*TODO*/ }, modifier = modifier) {
-        Column {
+    Card(
+        onClick = onItemClick,
+        modifier = modifier
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             AsyncImage(
-                model = imageURL,
+                model = item.image,
                 contentDescription = null,
             )
         }
         Spacer(modifier = Modifier.height(8.dp))
-        Text(text = name)
+        Text(
+            text = item.name,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.titleLarge
+        )
     }
 }
 
 @Preview
 @Composable
-fun AmiiboItemPreview(){
+fun AmiiboItemPreview() {
     PocketmiiboTheme {
-        AmiiboItem(name = "Filipe", imageURL = "https://cdn.awsli.com.br/2500x2500/138/138431/produto/11179849/11ffd2a092.jpg")
+        val amiibo = AmiiboUI(
+            type = "",
+            tail = "",
+            name = "Filipe",
+            image = "https://cdn.awsli.com.br/2500x2500/138/138431/produto/11179849/11ffd2a092.jpg",
+            head = "",
+            amiiboSeries = "",
+            character = "",
+            gameSeries = ""
+        )
+        AmiiboItem(item = amiibo) {
+
+        }
     }
 }
